fa154237040d58a1da8dc65c7cad6651544ea647
Sarah Boulfani <boulfanisarah@gmail.com>
2022-05-09 16:05:09 +0200

    Ajout helmet pour sécuriser les en-têtes HTTP

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,7 @@
-//Importer package dotenv pour utiliser les variables d'environnement
-require('dotenv').config();
-//console.log(process.env); // remove this after you've confirmed it working
-const sequelize = require('sequelize');
 //Importer path qui donne accés au chemin de notre systeme de fichiers
 const path = require('path');
+//Importer helmet
+const helmet = require("helmet");
 //Importer express
 const express = require('express');
 //Créer notre application express
@@ -11,30 +9,23 @@ const app = express();
 
 //Importer le router user
 const userRoutes = require('./routes/user');
-//Importer le router user
+//Importer le router post
 const postRoutes = require('./routes/post');
-//Importer le router user
+//Importer le router comment
 const commentRoutes = require('./routes/comment');
-
-//Import config
-const config = require('./config/config.json')
 //Import db
 const db = require('./models');
 
-
-
-
-//Synchronisation de la db
+//Synchronisation de la base de données
 db.sequelize.sync()//{force: true}
 .then((console.log("connexion à BDD reussie !")))
 .catch(error => console.log(error));
  
+//Utilisation de 'helmet' sur l'application 'express' 
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 
-var cors = require('cors');
 
-app.use(cors());
-
-//Ajout des headers pour accéder à notre API depuis n'importe quelle origine et avec les méthodes mentionnées. Le middleware ne prend pas d'adresse afin de s'appliquer à toutes les routes
+//Ajout des headers pour accéder à notre API depuis n'importe quelle origine et avec les méthodes mentionnées
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
@@ -48,13 +39,13 @@ app.use(express.json());
 //middleware used only for parsing request body of content-type x-www-form-urlencoded 
 app.use(express.urlencoded({ extended: true }));
 
-//Gérer la route vers le dossier images et traiter les requêtes vers la route /image. express.static gère de manière statique la ressource images à chaque requête vers la route images.
+//Gérer la route vers le dossier images 
 app.use('/images', express.static(path.join(__dirname, 'images')));
 //Enregister le router user
 app.use('/api/user', userRoutes);
-//Enregister le router user
+//Enregister le router post
 app.use('/api/post', postRoutes);
-//Enregister le router user
+//Enregister le router comment
 app.use('/api/comment', commentRoutes);
 
 
